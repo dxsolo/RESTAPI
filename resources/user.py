9c565980dfc2826352b7d93946a76568d09ac3e0
Dxsolo <dxsolo@gmail.com>
2018-12-07 09:13:25 -0500

    Adding TokenRefresh

--- resources/user.py
+++ resources/user.py
@@ -1,6 +1,11 @@
 from flask_restful import Resource, reqparse
 from werkzeug.security import safe_str_cmp
-from flask_jwt_extended import create_access_token, create_refresh_token
+from flask_jwt_extended import (
+	create_access_token, 
+	create_refresh_token,
+	jwt_refresh_token_required,
+	get_jwt_identity
+)
 from models.user import UserModel
 
 _user_parser = reqparse.RequestParser()
@@ -53,7 +58,7 @@ class UserLogin(Resource):
 		user = UserModel.find_by_username(data['username'])
 		# check password
 		if user and safe_str_cmp(user.password, data['password']):
-			access_token = create_access_token(identity = user.id, fresh = True)
+			access_token = create_access_token(identity = user.id, fresh=True)
 			refresh_token = create_refresh_token(user.id)
 			return {
 				'access_token': access_token,
@@ -64,3 +69,10 @@ class UserLogin(Resource):
 		# create access token
 		# create refresh token
 		# return them
+
+class TokenRefresh(Resource):
+	@jwt_refresh_token_required
+	def post(self):
+		current_user = get_jwt_identity()
+		new_token = create_access_token(identity=current_user, fresh=False)
+		return {'access_token': new_token}, 200
